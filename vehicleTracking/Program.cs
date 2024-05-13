53a9ea20662345eb66af55f2c300d89ee966c6cc
VernonDev <62961775+VernonDev@users.noreply.github.com>
2024-05-13 20:38:33 +0200

    Add project files.

---
+++ vehicleTracking/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace vehicleTracking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vehicle testVehicle = new Vehicle("Opel", "Corsa", 3, 85000);
+
+            Console.WriteLine("Car: " + testVehicle.Make + " " + testVehicle.Model
+
+              + " – R" + testVehicle.Price + ". Condition: " + testVehicle.Condition + "/5");
+        }
+    }
+}
---
+++ vehicleTracking/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vehicleTracking
+{
+    class Vehicle
+    {
+        // Variable declaration.
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Condition { get; set; }
+        public double Price { get; set; }
+
+
+        // Empty constructor.
+        public Vehicle()
+        {
+        }
+        // Constructor.
+        public Vehicle(string newMake, string newModel, int newCondition, double newPrice)
+        {
+            Make = newMake;
+            Model = newModel;
+            Condition = newCondition;
+            Price = newPrice;
+        }
+    }
+}
